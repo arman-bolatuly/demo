b896c20c33458981b1a152a621a88153ad937f1f
ArmanBulatovich <90092926+ArmanBulatovich@users.noreply.github.com>
2022-04-29 06:17:21 +0600

    second commit

--- pages/users/createuser.js
+++ pages/users/createuser.js
@@ -1,7 +1,50 @@
+import { useState } from "react";
+import useSwr from "swr";
+import axios from "axios";
+
+
+
 const Createuser = () => {
+  const url = "http://localhost:3001/api/users";
+  const fetcher = (url) => axios(url).then((res) => res.data);
+  const [author, setAuthor] = useState({
+    name: "",
+    surname: "",
+    date_of_birth: "",
+  });
+  const {data, mutate} = useSwr(url, fetcher);
+
+  const handlerSubmit = async (e) => {
+    e.preventDefault();
+    setAuthor("");
+    const newUser = {
+      name: author.name,
+      surname: author.surname,
+      date_of_birth: author.date_of_birth,
+    };
+    mutate([...data, newUser], false);
+    await axios.post(url, newUser);
+    mutate();
+  };
+
   return (
     <>
-      <h1>Createuser</h1>
+    <h1>Createuser</h1>
+    <form onSubmit={handlerSubmit}>
+      <input onChange={(e) => setAuthor(e.target.value)}
+        type="text"
+        id="name"
+        value={author.name}></input>
+        <input onChange={(e) => setAuthor(e.target.value)}
+        type="text"
+        id="surname"
+        value={author.surname}></input>
+        <input onChange={(e) => setAuthor(e.target.value)}
+        type="text"
+        id="date_of_birth"
+        value={author.date_of_birth}></input>
+      <button type="submit">Send</button>
+      </form>
     </>
   );
 };
--- pages/users/index.js
+++ pages/users/index.js
@@ -1,23 +1,44 @@
 import { useState } from "react";
 import useSWR from "swr";
 import axios from "axios";
-import expressService from "../../axiosConfig";
+//import expressService from "../../axiosConfig";
 
-const fetcher = (baseUrl) => axios.get(baseUrl).then((res) => res.data);
-
-const Users = () => {
-  const { data, error } = useSWR("users", () =>
-    exressService.get("/api/users").then((res) => res.data)
-  );
-
-  
-
+//const { data, error } = useSWR("users", () => exressService.get("/api/users").then((res) => res.data));
+  const Users = ({ users }) => {
+  const fetcher = (url) => axios.get("http://localhost:3001/api/users").then((res) => res.data);
+  const { data, error, mutate } = useSWR("/api/users", fetcher);
   if (error) return <div>ошибка загрузки</div>;
   if (!data) return <div>загрузка...</div>;
 
-  return <div>Привет, {data.name}!</div>;
+  const deleteUser = async(id) => {
+    await axios
+    .delete(`http://localhost:3001/api/users/${id}`)
+    await mutate()
+  }
+
+  return (
+  <>
+    Users:
+    <ul>
+    {users.map((user) => (
+      <li key={user.id}>Имя: {user.name}, Фамилия: {user.surname}, Дата рождения: {user.date_of_birth} <button onClick={() => deleteUser(user.id)}>Удалить</button></li>
+    ))
+    }
+    </ul>
+  </>
+  )
 };
 
+
+export async function getStaticProps() {
+  const users = await axios.get('http://localhost:3001/api/users').then((res) => res.data);
+  
+  return {
+    props: {
+      users,
+    },
+  }
+}
 export default Users;
 /*
   const fetchUsers = async () => {
